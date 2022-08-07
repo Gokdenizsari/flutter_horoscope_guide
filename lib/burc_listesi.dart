896e1220c6d948ade98ba3343933b0d07f548291
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-07 21:20:46 +0300

    horoscope

---
+++ lib/burc_listesi.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BurcListesi extends StatelessWidget {
+  const BurcListesi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Burc Listesi"),
+      ),
+      body :Center(
+      child: Text(""),
+    )
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_horoscope_guide/burc_listesi.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: BurcListesi(),
+    );
+  }
+}
