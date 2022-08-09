1265bd18e290560657d1790d5dcc0bd65a6df727
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-09 15:00:23 +0300

    Update burc_item.dart

--- lib/burc_item.dart
+++ lib/burc_item.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_horoscope_guide/burc_detay.dart';
 import 'package:flutter_horoscope_guide/model/burc.dart';
 
 class BurcItem extends StatelessWidget {
@@ -11,11 +12,25 @@ class BurcItem extends StatelessWidget {
   Widget build(BuildContext context) {
     var myTextStyle = Theme.of(context).textTheme;
     return Container(
-      color:Color.fromARGB(Random().nextInt(255), Random().nextInt(255), Random().nextInt(255), Random().nextInt(255)),
+      color: Color.fromARGB(Random().nextInt(255), Random().nextInt(255),
+          Random().nextInt(255), Random().nextInt(255)),
       child: ListTile(
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => BurcDetay(tiklanilanBurc: listelenenBurc),
+            ),
+          );
+        },
         leading: Image.asset("images/" + listelenenBurc.kucukResim),
-        title: Text(listelenenBurc.burcAdi,style: myTextStyle.headline5,),
-        subtitle: Text(listelenenBurc.burcTarihi,style: myTextStyle.subtitle1,),
+        title: Text(
+          listelenenBurc.burcAdi,
+          style: myTextStyle.headline5,
+        ),
+        subtitle: Text(
+          listelenenBurc.burcTarihi,
+          style: myTextStyle.subtitle1,
+        ),
         trailing: Icon(Icons.arrow_back_ios_new, color: Colors.red),
       ),
     );
