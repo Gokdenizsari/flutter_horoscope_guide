54dab0f1c9cd18cd935d3e2cce95b2f70aa0e997
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-09 14:10:57 +0300

    Update burc.dart

--- lib/model/burc.dart
+++ lib/model/burc.dart
@@ -5,5 +5,11 @@ class Burc {
   final String _kucukResim;
   final String _buyukResim;
 
-  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._kucukResim, this._buyukResim);
+  get burcAdi => this._burcAdi;
+  get burcTarihi => this._burcTarihi;
+  get burcDetayi => this._burcDetayi;
+  get kucukResim => this._kucukResim;
+  get buyukResim => this._buyukResim;
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._kucukResim,
+      this._buyukResim);
 }
