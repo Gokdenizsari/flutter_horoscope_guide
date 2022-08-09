e035e11b74d88cc0f5376579d95098fbc9194949
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-09 13:58:28 +0300

    Update burc_listesi.dart

--- lib/burc_listesi.dart
+++ lib/burc_listesi.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_horoscope_guide/data/strings.dart';
+import 'package:flutter_horoscope_guide/model/burc.dart';
+import '';
 
 class BurcListesi extends StatelessWidget {
-  const BurcListesi({Key? key}) : super(key: key);
+  List<Burc> tumBurclar = [];
+  BurcListesi() {
+    tumBurclar = veriKaynagi();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Burc Listesi"),
-      ),
-      body :Center(
-      child: Text(""),
-    )
+      appBar: AppBar(title: Text("Burclar Listesi")),
+      body: Center(child: ListView.builder(itemBuilder: (context, index) {
+        return Container();
+      })),
     );
+  }
+
+  List<Burc> veriKaynagi() {
+    List<Burc> gecici = [];
+    for (int i = 0; i < 12; i++) {
+      var burcAdi = Strings.BURC_ADLARI[i];
+      var burcTarihi = Strings.BURC_TARIHLERI[i];
+      var burcGenelOzellik = Strings.BURC_GENEL_OZELLIKLERI[i];
+      //Algoritma yeteneği geliştirmek için resimleri elde etme yöntemimiz.
+      //koc1.png oluşturmak için: Koc -->koc--->koc1.png
+      var kucukResim = Strings.BURC_ADLARI[i].toLowerCase() + "${i + 1}.png";
+      //Büyük resim için--> Koc--> koc_buyuk1.png
+      var buyukResim =
+          Strings.BURC_ADLARI[i].toLowerCase() + "_buyuk" + "${i + 1}.png";
+      Burc eklenecekBurc =
+          Burc(burcAdi, burcTarihi, burcGenelOzellik, kucukResim, buyukResim);
+      gecici.add(eklenecekBurc);
+    }
+
+    return gecici;
   }
 }
