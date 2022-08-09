83f8b05f2d6dc28caafb6cdb37af53c23f70e774
Gökdeniz SARİİBRAHİM <gkdnzsrbrhm@gmail.com>
2022-08-09 13:58:11 +0300

    Create burc.dart

---
+++ lib/model/burc.dart
@@ -0,0 +1,9 @@
+class Burc {
+  final String _burcAdi;
+  final String _burcTarihi;
+  final String _burcDetayi;
+  final String _kucukResim;
+  final String _buyukResim;
+
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._kucukResim, this._buyukResim);
+}
